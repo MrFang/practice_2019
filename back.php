1693a3e4e5a2cb49c69a9571b4df79be77893be1
Zolotukhin Fedor <bat312@yandex.ru>
2019-06-12 22:31:44 +0300

    Add edit functions

--- back.php
+++ back.php
@@ -1,26 +1,46 @@
 <?php
 
-    function getTypes($conn)
-    {
-        $res = mysqli_query($conn, 'SELECT * FROM content');
-        $result = array();
+function getTypes($conn) {
+    $res = mysqli_query($conn, 'SELECT * FROM DocumentTypes');
+    $result = array();
 
-        while($record = mysqli_fetch_array($res)) {
-            $result[] = $record;
-        }
-
-        return $result;
+    while($record = mysqli_fetch_array($res)) {
+        $result[] = $record;
     }
 
-    function addType($conn, $name, $description, $active) {
-        $name = '"'.$name.'"';
-        $active = $active == 'true' ? 1 : 0;
-        $description = $description == '' ? 'NULL' : '"'.$description.'"';
+    return $result;
+}
 
-        mysqli_query(
-            $conn,
-            'INSERT INTO DocumentTypes (name, description, ifActive) VALUES
-            ('.$name.', '.$description.', '.$active.')'
-        );
-    }
+function addType($conn, $name, $description, $active) {
+    $name = '"'.$name.'"';
+    $active = $active == 'true' ? 1 : 0;
+    $description = $description == '' ? 'NULL' : '"'.$description.'"';
+
+    mysqli_query(
+        $conn,
+        'INSERT INTO DocumentTypes (name, description, ifActive) VALUES
+        ('.$name.', '.$description.', '.$active.')'
+    );
+}
+
+function getTypeInfo($conn, $id) {
+    $res = mysqli_query(
+        $conn,
+        'SELECT * FROM DocumentTypes WHERE id='.$id    
+    );
+    
+    return mysqli_fetch_array($res);
+}
+
+function updateType($conn, $id, $name, $description, $active) {
+    $name = '"'.$name.'"';
+    $active = $active == 'true' ? 1 : 0;
+    $description = $description == '' ? 'NULL' : '"'.$description.'"';
+
+    mysqli_query(
+        $conn,
+        'UPDATE DocumentTypes SET name='.$name.', description='.$description.', ifActive='.$active.' WHERE id='.$id
+    );
+}
+
 ?>
--- front.php
+++ front.php
@@ -1,4 +1,5 @@
 <?php
+
 header('Content-Type: text/html; charset=utf-8');
 $conn = mysqli_connect('localhost', 'zolotukhin', 'fn9k5dkF', 'zolotukhin');
 mysqli_set_charset($conn, 'utf-8');
@@ -12,7 +13,7 @@ if (isset($_POST['action'])) {
             addType($conn, $_POST['name'], $_POST['description'], $_POST['active']);
             break;
         case 'update':
-            // TODO: Invoke update func
+            updateType($conn, $_POST['id'], $_POST['name'], $_POST['description'], $_POST['active']);
             break;
         case 'delete':
             // TODO: Invoke delete func
@@ -40,10 +41,20 @@ switch($page) {
 function renderDocumentTable($conn) {
     $records = getTypes($conn);
     $result = '<table class="table">';
-    $result.='<tr><td>id</td><td>text</td></tr>';
+    $result.='<tr><td>id</td><td>name</td><td>description</td><td>active</td><td>actions</td></tr>';
 
     for ($i = 0; $i < count($records); $i++) {
-        $result.='<tr><td>'.$records[$i]['id'].'</td><td>'.$records[$i]['text'].'</td></tr>';
+        $result.=
+        '<tr>'.
+        '<td>'.$records[$i]['id'].'</td>'.
+        '<td>'.$records[$i]['name'].'</td>'.
+        '<td>'.$records[$i]['description'].'</td>'.
+        '<td>'.$records[$i]['ifActive'].'</td>'.
+        '<td>'.
+            '<a href="./front.php?action=update&id='.$records[$i]['id'].'">edit</a><br/>'.
+            '<a href="./front.php?action=delete&id='.$records[$i]['id'].'">delete</a>'.
+        '</td>'.
+        '</tr>';
     }
 
     $result.='</table>';
@@ -52,13 +63,31 @@ function renderDocumentTable($conn) {
 }
 
 function renderAddForm() {
-    $result = '<form method="POST" action="./front.php">';
+    $result = '<form method="POST" action="./front.php?action=read">';
     $result.= '<input type="hidden" name="action" value="create">';
     $result.= 'NAME: <input type="text" name="name" required><br/>';
     $result.= 'DESCRIPRION: <textarea name="description"></textarea><br/>';
     $result.= 'ACTIVE: <select name="active" required><option>true</option><option>false</option></select><br/>';
     $result.= '<input type="submit" value="Add">';
     $result.='</form>';
+
+    return $result;
+}
+
+function renderEditForm($conn, $id) {
+    $item = getTypeInfo($conn, $id);
+
+    $result = '<form method="POST" action="./front.php?action=read">';
+    $result.= '<input type="hidden" name="action" value="update">';
+    $result.= '<input type="hidden" name="id" value="'.$id.'">';
+    $result.= 'NAME: <input type="text" name="name" required value="'.$item['name'].'"></br>';
+    $result.= 'DESCRIPTION: <textarea name="description">'.$item['description'].'</textarea></br>';
+    $result.= 'ACTIVE: <select name="active" required>'.
+        '<option>'.($item['ifActive'] == 1 ? 'true' : 'false').'</option>'.
+        '<option>'.($item['ifActive'] == 1 ? 'false' : 'true').'</option>'.
+    '</select><br/>';
+    $result.= '<input type="submit" value="Edit">';
+    $result.= '</form>';
 
     return $result;
 }
